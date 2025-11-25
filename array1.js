db4c6fefd41324788b179e30c3b4e0c84b0453b1
otaviogabriel-creato <otavio.gabriel@academico.ifpb.edu.br>
2025-11-25 20:08:19 -0300

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,12 @@
+const array = [4,12,8,30]
+
+let c = 0
+let t = array.length -1
+
+for (let i = 0;i <= t; i++){
+    array[i]
+    if(array[i] > 10){
+      c++
+    }
+  console.log("São maiores " + c + " números." 
+}
