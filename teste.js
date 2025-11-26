ad438b3912a34d605fb557599b409e1acfd7c5c7
otaviogabriel-creato <otavio.gabriel@academico.ifpb.edu.br>
2025-11-25 21:06:14 -0300

    array3.js

--- teste.js
+++ teste.js
@@ -1,1 +1,10 @@
-console.log("Hello word")
+const array = [10,5,7,3,2]
+let t = array.length
+let soma = 0
+
+for(let i = 0;i <= t;i+=2){
+    array[i]
+    soma += array[i]
+}
+
+console.log(soma);
