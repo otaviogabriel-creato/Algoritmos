f9560593e6fbaa886694ca9ad962e8a47569bd35
otaviogabriel-creato <otavio.gabriel@academico.ifpb.edu.br>
2025-11-25 21:34:55 -0300

    array5.js

---
+++ array5.js
@@ -0,0 +1,12 @@
+const array = [4,-2,10,-9,0,15]
+let t = array.length -1
+
+for(let i = 0;i <= t;i++){
+     array[i]
+    if(array[i] <= 0){
+        array[i] = 0
+      
+    }
+}
+
+  console.log(array);
