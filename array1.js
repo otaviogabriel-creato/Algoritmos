319dc3432702ed7a0adc0ee6cd5f49716555058f
otaviogabriel-creato <otavio.gabriel@academico.ifpb.edu.br>
2025-12-02 16:28:42 -0300

    array1.js

--- array1.js
+++ array1.js
@@ -1,9 +1,9 @@
 const array = [4,12,8,30,23]
 
 let c = 0
-let t = array.length -1
+let t = array.length 
 
-for (let i = 0;i <= t; i++){
+for (let i = 0;i < t; i++){
     array[i]
     if(array[i] > 10){
       c++
