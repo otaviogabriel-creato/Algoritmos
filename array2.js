314fa3adcb6700ad54a1d725e28c4f1af28eb148
otaviogabriel-creato <otavio.gabriel@academico.ifpb.edu.br>
2025-12-02 16:29:28 -0300

    array2.js

--- array2.js
+++ array2.js
@@ -1,8 +1,8 @@
 let a = ["sol","navigator","Margarida","pão"]
-let t = a.length -1
+let t = a.length 
 let a1 = []
 
-for (let i = 0;i <= t;i++){
+for (let i = 0;i < t;i++){
     let c = a[i].length
     if(c >= 5){
         a1.push(a[i])
